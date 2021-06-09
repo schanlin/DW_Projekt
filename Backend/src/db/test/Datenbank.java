2bb0273370b2adc441a20bb0cde2fc62f0d6b598
Belinda Schantong <71089883+beschantong@users.noreply.github.com>
2021-06-09 15:17:44 +0200

    backend grundlage erstellt

---
+++ Backend/src/db/test/Datenbank.java
@@ -0,0 +1,19 @@
+package db.test;
+
+import java.sql.*;
+
+public class Datenbank {
+	
+	public static void main(String[] args) {
+		String url = "jdbc:mysql://localhost:3306/test_db";
+		String user = "root";
+		String password = "Feuersturm11";
+		
+		try (Connection con = DriverManager.getConnection(url, user, password)){
+			System.out.println("Erfolgreich mit Datenbank verbunden.");
+		} catch(SQLException e) {
+			System.err.println(e.getMessage());
+		}
+	}
+
+}
