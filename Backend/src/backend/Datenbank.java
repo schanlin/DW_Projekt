88c67f08d633cf19269ff97da8e86d9b9a3ee692
Belinda Schantong <71089883+beschantong@users.noreply.github.com>
2021-06-11 19:41:12 +0200

    unnötige zweite main entfernt

--- Backend/src/backend/Datenbank.java
+++ Backend/src/backend/Datenbank.java
@@ -1,19 +1,7 @@
 package backend;
 
-import java.sql.*;
-
 public class Datenbank {
 	public static final String url = System.getProperty("dw.database.url");
 	public static final String user = System.getProperty("dw.database.user");
 	public static final String password = System.getProperty("dw.database.password");
-	
-	public static void main(String[] args) {
-		
-		try (Connection con = DriverManager.getConnection(url, user, password)){
-			System.out.println("Erfolgreich mit Datenbank verbunden.");
-		} catch(SQLException e) {
-			System.err.println(e.getMessage());
-		}
-	}
-
 }
