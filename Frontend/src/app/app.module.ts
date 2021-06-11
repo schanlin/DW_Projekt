18ae870ac316803342762ddbe89ec2df48ef275b
FelisPolarstern <usB+Ed1?>
2021-06-11 18:45:22 +0200

    Erste Schritte, Komponenten Navigation, Service Username

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { NavigationProfileComponent } from './navigation-profile/navigation-profile.component';
+import { NavigationLinksComponent } from './navigation-links/navigation-links.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    NavigationProfileComponent,
+    NavigationLinksComponent
   ],
   imports: [
     BrowserModule
