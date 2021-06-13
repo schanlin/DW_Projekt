ac0af550d663695a77dc9d66356f66811682e173
FelisPolarstern <felis.monstermietz@lf-net.org>
2021-06-13 16:35:58 +0200

    Klasse navigation-links angelegt, font awesome eingebunden

---
+++ Frontend/src/app/navigation-link/navigation-link.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-link',
+  templateUrl: './navigation-link.component.html',
+  styleUrls: ['./navigation-link.component.css']
+})
+export class NavigationLinkComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
